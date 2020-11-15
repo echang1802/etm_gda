4b485bf2c1b3351e4d70d4586239c110d5dffa32
echang1802 <41762304+echang1802@users.noreply.github.com>
2020-11-15 07:39:04 -0300

    args bug fixed

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 import argparse
 from classes import process
 
-if __main__ == '__main__':
+if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Etermax - Game Data Analyst')
-    parser.add_argument('--inputFilename', required=True, help='Directory and name of file with data to read')
-    parser.add_argument('--outputDirectory', required=True, help='Directory where to store the data')
-    parser.add_argument('--daysToSimulate', required=True, help='Number od days to simulate')
+    parser = argparse.ArgumentParser(description = 'Etermax - Game Data Analyst')
+    parser.add_argument('--inputFilename', required = True, help = 'Directory and name of file with data to read')
+    parser.add_argument('--outputDirectory', required = True, help = 'Directory where to store the data')
+    parser.add_argument('--daysToSimulate', default = 10, help = 'Number of days to simulate')
     args = parser.parse_args()
 
     proc = process(args.inputFilename, args.outputDirectory)
